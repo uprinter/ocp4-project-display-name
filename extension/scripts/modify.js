21bc436b9f12f0810cd3ea7a586628cd9619275e
uprinter <kosharovskiy@gmail.com>
2022-12-06 15:08:18 +0200

    Fixed dropdown menu items

--- extension/scripts/modify.js
+++ extension/scripts/modify.js
@@ -3,15 +3,28 @@ const projects = {
 };
 
 setInterval(() => {
-    let elements = document.querySelectorAll('button.co-namespace-dropdown__menu-toggle > span.pf-c-menu-toggle__text');
+    let dropDownMenu = document.querySelectorAll('button.co-namespace-dropdown__menu-toggle > span.pf-c-menu-toggle__text');
 
-    if (elements.length > 0) {
-        let text = elements[0].innerHTML;
+    if (dropDownMenu.length > 0) {
+        let text = dropDownMenu[0].innerHTML;
         let projectId = text.replace('Project: ', '')
 
         if (projects[projectId] != null) {
-            elements[0].innerHTML = text.replace(projectId, projectId + ' (' + projects[projectId] + ')')
-        }
+            dropDownMenu[0].innerHTML = text.replace(projectId, projectId + ' (' + projects[projectId] + ')')
 
+        }
+    }
+
+    let dropDownMenuItems = document.querySelectorAll('ul.pf-c-menu__list > li.pf-c-menu__list-item > button.pf-c-menu__item > span > span');
+
+    if (dropDownMenuItems.length > 0) {
+        if (dropDownMenuItems.length > 0) {
+            dropDownMenuItems.forEach(item => {
+                let projectId = item.innerHTML;
+                if (projects[projectId] != null) {
+                    item.innerHTML = projectId + ' (' + projects[projectId] + ')';
+                }
+            });
+        }
     }
 }, 1000)
